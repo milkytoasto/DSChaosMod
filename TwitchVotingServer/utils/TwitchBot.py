1dc7dd0062c74eb6ef425892ec8d2e4d37d88743
MILKIETOAST <milkietoast@gmail.com>
2022-09-18 00:19:32 -0700

    added vote tracking

--- TwitchVotingServer/utils/TwitchBot.py
+++ TwitchVotingServer/utils/TwitchBot.py
@@ -2,14 +2,26 @@ from twitchio.ext import commands
 
 
 class TwitchBot(commands.Bot):
-    def __init__(self, token, channel, debug_logger, chat_logger, messageHandler=False):
+    def __init__(
+        self,
+        token,
+        channel,
+        debug_logger,
+        chat_logger,
+        messageHandler=False,
+        numOptions=3,
+    ):
         super().__init__(token=token, prefix="?", initial_channels=[channel])
         self.voting = False
-        self.messages = []
         self.messageHandler = messageHandler
         self.debug_logger = debug_logger
         self.chat_logger = chat_logger
         self.channel = channel
+        self.optionKeys = [str(val + 1) for val in range(numOptions)]
+        self.initVotes()
+
+    def initVotes(self):
+        self.votes = {key: set() for key in self.optionKeys}
 
     async def event_ready(self):
         self.debug_logger.info(f"TwitchBot: Logged onto Twitch WS as {self.nick}")
@@ -24,6 +36,8 @@ class TwitchBot(commands.Bot):
 
         self.chat_logger.info(f"{author.name}: {content}")
 
-        self.messages.append(content)
-        if self.messageHandler:
-            self.messageHandler(message.content)
+        if message.content in self.votes.keys():
+            self.votes[message.content].add(message.author.name)
+
+            if self.messageHandler:
+                self.messageHandler(self.votes)
