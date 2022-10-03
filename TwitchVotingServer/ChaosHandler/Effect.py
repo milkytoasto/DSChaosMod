3e4d6144da2cbdb7d482f4ba933181e81968f3d8
MILKIETOAST <milkietoast@gmail.com>
2022-10-02 18:07:26 -0700

    reworked BaseEffect to utilize a running class variable

--- TwitchVotingServer/ChaosHandler/Effect.py
+++ TwitchVotingServer/ChaosHandler/Effect.py
@@ -1,11 +1,24 @@
 class BaseEffect:
     name = "Base Effect"
+    running = False
     seconds: int
 
     @classmethod
     async def start(cls, *args):
+        if not cls.running:
+            cls.running = True
+            await cls.onStart(*args)
+
+    @classmethod
+    async def onStart(cls, *args):
         pass
 
     @classmethod
     async def stop(cls, *args):
+        if cls.running:
+            await cls.onStop(*args)
+            cls.running = False
+
+    @classmethod
+    async def onStop(cls, *args):
         pass
