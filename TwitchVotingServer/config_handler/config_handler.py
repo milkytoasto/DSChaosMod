ec460725ef7c1743ce0935392373098e5b482b92
MILKIETOAST <milkietoast@gmail.com>
2023-03-20 23:45:49 -0700

    Updating config handler for toml conversion

--- TwitchVotingServer/config_handler/config_handler.py
+++ TwitchVotingServer/config_handler/config_handler.py
@@ -1,4 +1,6 @@
-import configparser
+import tkinter as tk
+
+import toml
 
 
 class ConfigHandler:
@@ -6,29 +8,39 @@ class ConfigHandler:
         self.config_path = config_path
 
         with open(self.config_path) as f:
-            self.config = configparser.ConfigParser()
-            self.config.read_file(f)
+            self.config = toml.load(f)
 
     def save_config(self, fields):
-        for section in fields:
-            for option in fields[section]:
-                if (value := fields[section][option].get()) is not None:
-                    self.config.set(section, option, str(value))
+        new_config = self.config.copy()
+        for section_name, section_fields in fields.items():
+            for field_name, field_value in section_fields.items():
+                if isinstance(field_value, tk.BooleanVar):
+                    new_config[section_name][field_name] = bool(field_value.get() == 1)
+                else:
+                    new_config[section_name][field_name] = field_value.get()
 
-        with open(self.config_path, "w") as configfile:
-            self.config.write(configfile)
+        with open(self.config_path, "w") as f:
+            toml.dump(new_config, f)
 
     def get_section(self, section):
         return self.config[section] if section in self.config else None
 
     def get_option(self, section, option, default_value, type=str):
-        if self.config.has_option(section, option):
-            if type is str:
-                return self.config[section][option]
-            if type is bool:
-                return self.config[section].getboolean(option)
-            if type is int:
-                return self.config[section].getint(option)
-            if type is float:
-                return self.config[section].getfloat(option)
+        if section in self.config and option in self.config[section]:
+            return self.config[section][option]
         return default_value
+
+    def get(self, option_name, default_value=None, type=str):
+        option_parts = option_name.split(".")
+        option_value = self.config
+        for part in option_parts:
+            if part in option_value:
+                option_value = option_value[part]
+            else:
+                return default_value
+        if option_value is None:
+            return default_value
+        if type == bool:
+            return option_value.lower() == "true"
+        else:
+            return type(option_value)
